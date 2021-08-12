ff3c8950c6b0185d0bdb02fd23b25d6819abcd57
syl <lvnfmsalthl@naver.com>
2021-08-12 14:41:20 +0900

    commit이소영

--- app/src/main/java/com/example/smartdrive/MainActivity.java
+++ app/src/main/java/com/example/smartdrive/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.smartdrive;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    final static String TAG = "CROCUS";
+    float prevX, prevY;
+
+    ImageView mImageView = findViewById(R.id.malpungsun_1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +27,37 @@ public class MainActivity extends AppCompatActivity {
 
         //startActivity(intent);
     }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        mImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        prevX = motionEvent.getX();
+                        prevY = motionEvent.getY();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        float dx = motionEvent.getX() - prevX;
+                        float dy = motionEvent.getY() - prevY;
+                        Log.v(TAG, "dx : " + dx + " dy :: " + dy);
+                        view.setX(motionEvent.getX());
+                        view.setY(motionEvent.getY());
+                        break;
+
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return true;
+
+            }
+        });
+    }
+
 }
