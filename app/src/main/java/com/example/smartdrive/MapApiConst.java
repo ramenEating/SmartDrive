df6b3c62478db22d395ed010f7ee9d1c1ecc6666
b1uem00n <nyoung0301@naver.com>
2021-08-21 23:55:01 +0900

    searchable.xml/MapApiCont.class 등 지도 검색(searchview) 부분

---
+++ app/src/main/java/com/example/smartdrive/MapApiConst.java
@@ -0,0 +1,5 @@
+package com.example.smartdrive;
+
+public class MapApiConst {
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "f43c0c8e749ea4a8583ddfcaf6a22472";
+}
--- app/src/main/java/com/example/smartdrive/MapParking.java
+++ app/src/main/java/com/example/smartdrive/MapParking.java
@@ -2,33 +2,91 @@ package com.example.smartdrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
-
-import net.daum.android.map.MapViewController;
+import android.widget.Button;
+import android.widget.SearchView;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public class MapParking extends AppCompatActivity {
+    Button searchBT;
+    private SearchView mSearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_parking);
 
         initView();
+        handleIntent(getIntent());
     }
-
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            mSearchView.setQuery(mSearchView.getQuery(), true);
+            //use the query to search your data somehow
+        }
+    }
     private void initView() {
+        //지도
         MapView mapView = new MapView(this);
-
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+        // 줌 레벨 변경
+        mapView.setZoomLevel(7, true);
+        // 중심점 변경 + 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
+        // 줌 인
+        mapView.zoomIn(true);
+        // 줌 아웃
+        mapView.zoomOut(true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+
+
+        //검색(서치뷰)
+        mSearchView = findViewById(R.id.searchView); //searchView
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // 검색 버튼이 눌러졌을 때 이벤트 처리
+                mSearchView.setQuery(mSearchView.getQuery(), true);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // 검색어가 변경되었을 때 이벤트 처리
+                mSearchView.setQuery(mSearchView.getQuery(), false);
+                return false;
+            }
+        });
+
+        //개별 검색 버튼
+        searchBT = (Button) findViewById(R.id.buttonSearch);
+        searchBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //검색처리코드(???)
+                mSearchView.setQuery(mSearchView.getQuery(), true);
+            }
+        });
     }
 }
