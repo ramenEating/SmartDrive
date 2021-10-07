f73b50a02ec9a73b86c7e0b7f9daf277f3e41a23
ramenEating <cocouki09@naver.com>
2021-10-07 16:33:11 +0900

    은주 소영 검색인데 업뎃하지 말아보셈

--- app/src/main/java/com/example/smartdrive/MapGas.java
+++ app/src/main/java/com/example/smartdrive/MapGas.java
@@ -10,6 +10,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,29 +22,52 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import net.daum.mf.map.api.MapView;
 
-public class MapGas extends AppCompatActivity implements OnMapReadyCallback {
+
+
+
+public class MapGas extends AppCompatActivity {
 
     private GoogleMap mMap;
-    Button chatbotBt, parkingBt;
+    private static final int PLACE_PICKER_REQUEST =1;
+
+    Button chatbotBt, parkingBt, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapgas_miniinfo);
 
+        button3 = (Button)findViewById(R.id.button3);
         chatbotBt = (Button)findViewById(R.id.chatbot);
         parkingBt = (Button)findViewById(R.id.parking);
 
+
+        /*
         //googleMap 객체가 준비될때 실행될 콜백 메소드
         SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_view);
         mapFragment.getMapAsync(this); //메인 스레드에서 호출되어야 onMapReady 콜백 실행
-
+*/
 /*
         //지도(뷰 추가): 메인 뷰 그룹에 코드를 통해 add 해야 함
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 */
+        button3.setOnClickListener(new Button.OnClickListener()
+        { @Override
+        public void onClick(View view)
+        {
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            try {
+                Intent intent = intentBuilder.build(MapGas.this);
+                startActivityForResult(intent,PLACE_PICKER_REQUEST);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
+        }
+        });
 
 
         chatbotBt.setOnClickListener(new View.OnClickListener(){
@@ -62,6 +88,8 @@ public class MapGas extends AppCompatActivity implements OnMapReadyCallback {
     }
 
     //맵이 사용할 준비 되면 (NULL이 아닌 googleMap 객체를 파라미터로 제공해 줄 수 있을때) 호출
+   /*
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
@@ -75,15 +103,16 @@ public class MapGas extends AppCompatActivity implements OnMapReadyCallback {
         markerOptions.snippet("한국의 수도");  //클릭 시 보여주는 설명
         mMap.addMarker(markerOptions); //googleMap 객체에 추가해주면 지도에 표시
 
-        /*
+
         //moveCamera 카메라를 지정한 위도, 경도 이동
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(10)); //지정한 단계로 카메라 줌 조정. 1단계=세계지도 수준. 숫자 클수록 상세지도
-*/
+
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
 
 
 
     }
+    */
 }
