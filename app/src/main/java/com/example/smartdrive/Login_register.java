f431597ae8abb9ad326beec780e5bf5c98d289ff
b1uem00n <nyoung0301@naver.com>
2021-10-16 21:46:01 +0900

    (+)주석까지 찐막 업데이트

--- app/src/main/java/com/example/smartdrive/Login_register.java
+++ app/src/main/java/com/example/smartdrive/Login_register.java
@@ -32,17 +32,26 @@ public class Login_register extends AppCompatActivity {
 
                     //jsp와 통신 작업 클래스 생성 후 불러오기
                     Login_registerActivity task = new Login_registerActivity();
-                    result = task.execute(email, id, type).get(); //통신 성공 후 받은 데이터
+
+                    result = task.execute(email, id, type).get();
+                    //result = 통신 성공 후 받은 데이터
+                    //But, joinO!<!DOCTYPE html>< ... 이렇게 모든 jsp 문자까지 넘어옴
+
+                    // "!" 기준으로 앞부분만 가져오기 작업(그래서 이클립스에도 일부러 ! 넣음)
                     String rr = result.substring(0, result.indexOf("!"));
+                    //이렇게 하면 joinO, joinX 만 가져올 수 있음
+
                     //가입 여부
                     if(rr.equals("joinO")) {
                         Toast.makeText(Login_register.this, "가입 성공", Toast.LENGTH_SHORT).show();
                         //가입 성공이면 로그인 페이지로 넘어가기
                         startActivity(new Intent(Login_register.this, Login_member.class));
                     }
-                    else if(rr.equals("joinX")) Toast.makeText(Login_register.this, "이미 존재하는 이메일입니다", Toast.LENGTH_SHORT).show();
-                    else Toast.makeText(Login_register.this, "네트워크 연결 문제: 가입할 수 없음", Toast.LENGTH_SHORT).show();
-                    //Log.i("WOW", rr);
+                    else if(rr.equals("joinX"))
+                        Toast.makeText(Login_register.this, "이미 존재하는 이메일입니다", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(Login_register.this, "네트워크 연결 문제: 가입할 수 없음", Toast.LENGTH_SHORT).show();
+                    //Log.i("WOW", rr); 로그캣에서 출력 가능
                 } catch (Exception e) {
                     //Log.i("DBtest", ".....ERROR.....!");
                 }
